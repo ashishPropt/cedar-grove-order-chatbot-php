1a041073f105fbd92f197cdd28da298cb59e767a
ashishPropt <ashish@proptxchange.com>
2026-05-10 15:13:53 -0400

    test: trigger deploy to verify auto web root detection

--- public/test.php
+++ public/test.php
@@ -1,4 +1,1 @@
-<?php
-echo '<h2 style="font-family:sans-serif;color:green">&#10003; FTP Deploy Working!</h2>';
-echo '<p style="font-family:sans-serif">Deployed at: ' . date('Y-m-d H:i:s') . ' UTC</p>';
-echo '<p style="font-family:sans-serif">PHP version: ' . PHP_VERSION . '</p>';
+<?php echo '<h2 style="font-family:sans-serif;color:green">&#10003; Deploy test 2 - ' . date('Y-m-d H:i:s') . ' UTC</h2>';
