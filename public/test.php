0d9c2292b373aad86d9b459f169c9aa869a60aab
ashishPropt <ashish@proptxchange.com>
2026-05-10 15:11:21 -0400

    test: update test.php to trigger FTP deploy workflow

--- public/test.php
+++ public/test.php
@@ -1,5 +1,4 @@
 <?php
-echo "<h2>FTP Deploy Test</h2>";
-echo "<p>Deployed at: " . date('Y-m-d H:i:s') . " UTC</p>";
-echo "<p>PHP version: " . PHP_VERSION . "</p>";
-echo "<p style='color:green'>&#10003; FTP deploy is working!</p>";
+echo '<h2 style="font-family:sans-serif;color:green">&#10003; FTP Deploy Working!</h2>';
+echo '<p style="font-family:sans-serif">Deployed at: ' . date('Y-m-d H:i:s') . ' UTC</p>';
+echo '<p style="font-family:sans-serif">PHP version: ' . PHP_VERSION . '</p>';
