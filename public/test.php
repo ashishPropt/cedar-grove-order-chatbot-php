e56b04956c1fb2d82a584f97ac1970d31315bc57
ashishPropt <ashish@proptxchange.com>
2026-05-10 09:46:46 -0400

    test: add test.php to verify FTP deploy is working

---
+++ public/test.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2>FTP Deploy Test</h2>";
+echo "<p>Deployed at: " . date('Y-m-d H:i:s') . " UTC</p>";
+echo "<p>PHP version: " . PHP_VERSION . "</p>";
+echo "<p style='color:green'>&#10003; FTP deploy is working!</p>";
